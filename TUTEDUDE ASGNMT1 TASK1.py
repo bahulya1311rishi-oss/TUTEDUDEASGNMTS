b5a83b0403c89088b6700d48736697d5747d6ac1
bahulya1311rishi-oss <bahulya1311rishi@gmail.com>
2025-10-02 07:56:53 +0530

    Add files via upload

---
+++ TUTEDUDE ASGNMT1 TASK1.py
@@ -0,0 +1,14 @@
+print(' welcome to task1 of assignment')
+a=input('PLease enter first number')
+b=input('PLease enter second number')
+a=float(a)
+b=float(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print('ADDITION:',c)
+print('SUBTRACTION:',d)
+print('MULTIPLICATION:',e)
+print('DIVISON:',f)
+print('thanks')
---
+++ TUTEDUDE ASGNT1 TASK2.py
@@ -0,0 +1,4 @@
+a=input('please enter your first name')
+b=input('please enter your last name')
+print('Hey' + a + b+'!'+' WELCOME TO THE PYTHON WORLD')
+
