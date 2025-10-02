87db1020168fbc070df22503122f81f86cbc425b
bahulya1311rishi-oss <bahulya1311rishi@gmail.com>
2025-10-02 19:06:05 +0530

    Add files via upload

---
+++ TUTEDUDE ASGNMT2 TASK1(better).py
@@ -0,0 +1,5 @@
+x=int(input('Kindly enter your number: '))
+if x%2==0:
+        print(str(x) + 'is an even number')
+else:
+        print(str(x) + ' is an odd number')
---
+++ TUTEDUDE ASGNMT2 TASK2.py
@@ -0,0 +1,5 @@
+l=range(1,51)
+for i in l:
+    print(i)
+print('Sum of numbers from '+  str(1) + ' to ' +  str(50) + ' is: ', sum(l))
+
